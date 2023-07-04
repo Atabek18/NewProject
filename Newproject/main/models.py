659e1d87919c9c4b17900a2720cb7b3ff6d7af00
Atabek <atabekabduakimov1806@gmail.com>
2023-07-04 22:19:33 +0500

    News

--- Newproject/main/models.py
+++ Newproject/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class New_preject(models.Model):
+    file_name = models.TextField('file_name')
+    file_size = models.IntegerField('file_size')
+    format_file = models.CharField('format_file', max_length=50)
+
+    def __str__(self):
+        return self.file_name
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
